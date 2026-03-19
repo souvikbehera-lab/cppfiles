1a4700387753fa106e9678fca5e0c680293fefa2
A.Souvik_behera <asouvikbehera@gmail.com>
2026-03-19 20:17:48 +0530

    patterns

---
+++ pattern_printing/01_star_rectangle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number of rows : ";
+    cin>>n;
+    for(int i=1; i<=n; i++){//rows
+       for(int j=1; j<=n; j++){//columns
+           cout<<j;
+           
+           
+        }
+    cout<<endl;
+    }
+}
